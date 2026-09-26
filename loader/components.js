4e4775218bea3f4c956138d699009b2e1db0b950
lizvibecodes <lizeldavonfrancois@gmail.com>
2026-09-26 23:04:21 +0200

    latest changes

--- loader/components.js
+++ loader/components.js
@@ -17,7 +17,10 @@ loadComponent("category_strip", "parts/category_strip.html");
 loadComponent("footer", "parts/footer.html");
 loadComponent("head", "parts/head.html");
 loadComponent("hero", "parts/hero.html");
-loadComponent("home", "parts/home.html");
 loadComponent("role_bar", "parts/role_bar.html");
 loadComponent("style", "parts/style.html");
 loadComponent("top_bar", "parts/top_bar.html");
+
+
+loadComponent("home", "parts/home.html");
+loadComponent("ploog", "parts/ploog.html");
